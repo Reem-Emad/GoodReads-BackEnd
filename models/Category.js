63a157dc84e7723ea37f58bbeb3635f48f7c6a84
asmaa <asmaaatahaa@gmail.com>
2019-04-26 19:36:12 +0200

    add category population

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,23 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
+const bookModel = require('./Book');
 
 const categorySchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true
-    }
-});
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    books: [{
+        type: String,
+        ref: bookModel,
+        required: true
+    }]
+}, { toJSON: { virtuals: true } });
+categorySchema.virtual('bookData', {
+    ref: 'Book',
+    localField: 'name',
+    foreignField: 'category',
 
-const categoryModel = mongoose.model('Category',categorySchema);
+});
+const categoryModel = mongoose.model('Category', categorySchema);
 module.exports = categoryModel;
